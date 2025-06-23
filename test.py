fb0ece744298e89300df9edd62525d1cc1203a16
rana eyp <eyrana002@gmail.com>
2025-06-23 22:24:14 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,52 @@
+# isim = input("Adını gir: ")
+# print("Merhaba, " + isim + "!")
+
+# a=int(input("sayiyı gir: "))
+# b=int(input("sayiyi gir: "))
+# print("sum :",a+b)
+
+# tek çift kontrolü
+# numb=int(input("please enter a number: "))
+# if(numb%2==0):
+#     print("cift")
+# else: print("tek")
+
+# pozatif negatif kontrolü
+# numb=int(input("Please enter a number: "))
+# if( numb>0):
+#     print("Positive")
+# elif( numb<0):
+#     print("Negative")
+# else:
+#     print("Zero")
+
+# nott = int(input("Lütfen notunuzu giriniz: "))
+# if nott >= 90 and nott <= 100:
+#     print("Puanınız: AA")
+# elif nott >= 80:
+#     print("Puanınız: BA")
+# elif nott >= 70:
+#     print("Puanınız: BB")
+# elif nott >= 60:
+#     print("Puanınız: CB")
+# elif nott >= 50:
+#     print("Puanınız: CC")
+# elif nott >= 0:
+#     print("Kaldınız")
+# else:
+#     print("Geçersiz not girdiniz.")
+
+# sayi = 1
+# while sayi <= 10:
+#     print(sayi)
+#     sayi += 1
+ 
+# toplam = 0
+# i = 0
+# while True:  # sürekli çalışsın ama kesinlikle break kullan
+#     sayi = int(input("Sayı gir (0 girersen durur): "))
+#     if sayi == 0:
+#         break
+#     toplam += sayi
+#     i += 1
+# print("Toplam:", toplam)
