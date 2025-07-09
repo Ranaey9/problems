8cb6c643add99a8d005feedf2e7b32cf3d48f601
rana eyp <eyrana002@gmail.com>
2025-07-09 23:11:40 +0300

    Add files via upload

---
+++ problems_1.sayfa/1_soru.py
@@ -0,0 +1,6 @@
+# Girilen sayının pozitif ya da negatif olduğunu ekrana yazınız.
+numb=int(input("please enter a number"))
+if numb<0:
+    print("negative")
+elif numb>0:
+    print("posative")
---
+++ problems_1.sayfa/2_soru.py
@@ -0,0 +1,6 @@
+# Girilen sayının mutlak değerini alan algoritma
+numb=int(input("please enter a number"))
+if numb<0:
+    print("mutlak:", -numb)
+elif numb>0:
+    print("mutlak:", numb)
