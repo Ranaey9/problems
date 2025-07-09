f5b028ce8c992dcbcefbcf82c416e4f3e4c128a0
rana eyp <eyrana002@gmail.com>
2025-07-09 23:20:17 +0300

    Add files via upload

---
+++ problems_1.sayfa/4_soru.py
@@ -0,0 +1,6 @@
+# Girilen sayının 6'nın katı olup olmadığını bulan algoritma?
+numb=int (input("Lütfen bir sayı giriniz: "))
+if numb%6==0:
+    print("6'nın katı")
+else:
+    print("6'nın katı değil")
